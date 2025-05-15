42a27374b60f6c602ad6907c58cab07c5d415d12
DabSaint <122949033+jnotsknab@users.noreply.github.com>
2025-05-15 02:46:58 -0500

    More Security Header Stuff

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,7 @@ const nextConfig = {
                 { key: 'X-Frame-Options', value: 'DENY' },
                 { key: 'X-Content-Type-Options', value: 'nosniff' },
                 { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-                { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+                { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains' },
                 { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
                 { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
                 { key: 'X-DNS-Prefetch-Control', value: 'on' },
