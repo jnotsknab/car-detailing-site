ca31f564ad8dc9e74838264753a5578b09d48371
DabSaint <122949033+jnotsknab@users.noreply.github.com>
2025-05-14 20:32:14 -0500

    Fixed Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${titillium.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
